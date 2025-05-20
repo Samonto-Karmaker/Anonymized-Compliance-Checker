fdc6dea736b729d9448fc13c69902712d7397dc4
omitul.islam <omitul.islam@brainstation-23.com>
2025-05-20 15:17:11 +0600

    added the db module to the app module

--- anonymized-compliance-checker-web2/src/app.module.ts
+++ anonymized-compliance-checker-web2/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from "./app.service"
 import { Task1Module } from "./task1/task1.module"
 import { Task2Module } from "./task2/task2.module"
 import { Task3Module } from "./task3/task3.module"
+import { DatabaseModule } from "./db/db.module"
 
 @Module({
-    imports: [Task1Module, Task2Module, Task3Module],
+    imports: [ DatabaseModule ,Task1Module, Task2Module, Task3Module],
     controllers: [AppController],
     providers: [AppService],
 })
