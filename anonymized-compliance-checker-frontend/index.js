f594814a5edf9d7799b2a3154de5a29e1a200c73
omitul.islam <omitul.islam@brainstation-23.com>
2025-05-22 16:12:16 +0600

    modified index.js
    added a spinner and modified the code a little bit

--- anonymized-compliance-checker-frontend/index.js
+++ anonymized-compliance-checker-frontend/index.js
@@ -10,6 +10,11 @@ task2CheckBtn.onclick = checkTask2;
 const backend_url = "http://localhost:3000";
 
 async function checkTask1() {
+    task1CheckBtn.disabled = true;
+    task1CheckBtn.innerHTML = `
+      <span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>
+      Checking...
+    `;
     try {
         const response = await fetch(`${backend_url}/task1/check-compliance`);
         const data = await response.json();
@@ -21,11 +26,20 @@ async function checkTask1() {
         }
     } catch (error) {
         console.log("Task1 Error => ", error);
-        result1.innerHTML = "Interval Server Error!";
+        result1.innerHTML = "Internal Server Error!";
+    } finally {
+        task1CheckBtn.disabled = false;
+        task1CheckBtn.innerHTML = "Check";
     }
 }
 
+
 async function checkTask2() {
+    task2CheckBtn.disabled = true;
+    task2CheckBtn.innerHTML = `
+      <span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>
+      Checking...
+    `;
     try {
         const batchSize = parseInt(
             document.getElementById("batch-size").value || 5,
@@ -40,7 +54,10 @@ async function checkTask2() {
             result2.innerHTML = `<b>Result: </b><span class="text-danger">${data.msg}</span>`;
         }
     } catch (error) {
-        console.log("Task1 Error => ", error);
-        result2.innerHTML = "Interval Server Error!";
+        console.log("Task2 Error => ", error);
+        result2.innerHTML = "Internal Server Error!";
+    } finally {
+        task2CheckBtn.disabled = false;
+        task2CheckBtn.innerHTML = "Check";
     }
 }
