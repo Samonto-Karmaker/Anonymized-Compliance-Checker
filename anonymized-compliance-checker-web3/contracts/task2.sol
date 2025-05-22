0400814c680b79c26818bc727ebb586c4b9c95d9
AlFahimBinFaruk <alfahim.ps@gmail.com>
2025-05-22 12:58:02 +0600

    feat:made smart contract more gas efficient, tested everything

--- anonymized-compliance-checker-web3/contracts/task2.sol
+++ anonymized-compliance-checker-web3/contracts/task2.sol
@@ -6,13 +6,14 @@ pragma solidity ^0.8.28;
 /// @notice Validates that each expiry date is at least 6 months after the disbursed date
 /// @dev Uses a fixed time difference of 6 months (15552000 seconds)
 
+
+/// @notice Error thrown when the expiry date is not more than 6 months after the disbursed date
+error TaskTwo_RulesViolated();
+
 contract Task2 {
 
-    /// @notice Error thrown when the expiry date is not more than 6 months after the disbursed date
-    error TaskTwo_RulesViolated();
-
     /// @notice The minimum required time difference between disbursed and expiry dates (6 months)
-    uint256 timeDifferance = 15552000; // 6 months in seconds
+    uint256 constant timeDifference = 15552000; // 6 months in seconds
 
     /// @notice Structure to represent disbursed and expiry dates
     /// @param expiryDate The date when the item or policy expires (as a Unix timestamp)
@@ -25,11 +26,11 @@ contract Task2 {
     /// @notice Validates that the expiry date is more than 6 months after the disbursed date for each item
     /// @dev Reverts with TaskTwo_RulesViolated if any date pair violates the rule
     /// @param dates An array of DateObj structs containing expiry and disbursed dates
-    function validate(DateObj[] memory dates) external view {
+    function validate(DateObj[] memory dates) external pure {
         uint256 n = dates.length;
         for (uint256 i = 0; i < n; i++) {
             uint256 diff = dates[i].expiryDate - dates[i].disbursedDate;
-            if (diff <= timeDifferance) {
+            if (diff <= timeDifference) {
                 revert TaskTwo_RulesViolated();
             }
         }
