fb1a39a8a83cccd5a5ae5783e101d79ea15ec8bb
Samonto Karmaker <samonto0726@gmail.com>
2025-05-23 14:43:03 +0600

    feat: add Task3 contract module and service with ABI integration

--- anonymized-compliance-checker-web2/src/task3/task3.module.ts
+++ anonymized-compliance-checker-web2/src/task3/task3.module.ts
@@ -5,9 +5,13 @@ import { TypeOrmModule } from "@nestjs/typeorm"
 import { Inventory } from "src/db/inventory.entity"
 import { BatchInfo } from "src/db/batchInfo.entity"
 import { CronService } from "./task3-cron/cron.service"
+import { Task1ContractModule } from "src/contracts/task1/task1.contract.module"
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Inventory, BatchInfo])],
+    imports: [
+        TypeOrmModule.forFeature([Inventory, BatchInfo]),
+        Task1ContractModule,
+    ],
     controllers: [Task3Controller],
     providers: [InventoryService, CronService],
 })
