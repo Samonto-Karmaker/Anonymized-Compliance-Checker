6ed183de7099ab09d43f0197625be97a3f6df3a2
Samonto Karmaker <samonto0726@gmail.com>
2025-05-23 12:22:22 +0600

    feat: implement cron job for dynamic periodic task synchronization

--- anonymized-compliance-checker-web2/src/task3/task3.service.ts
+++ anonymized-compliance-checker-web2/src/task3/task3.service.ts
@@ -3,21 +3,24 @@ import {
     Injectable,
     InternalServerErrorException,
     NotFoundException,
+    OnModuleInit,
 } from "@nestjs/common"
 import { Repository } from "typeorm"
 import { InjectRepository } from "@nestjs/typeorm"
 import { Inventory } from "src/db/inventory.entity"
 import { BatchInfo } from "src/db/batchInfo.entity"
 import { CreateInventoryDto } from "src/dto/create-inventory"
-import { Cron, CronExpression } from "@nestjs/schedule"
+import { CronJob } from "cron"
+import { CronExpression, SchedulerRegistry } from "@nestjs/schedule"
 
 @Injectable()
-export class Task3Service {
+export class Task3Service implements OnModuleInit {
     constructor(
         @InjectRepository(Inventory)
         private readonly inventoryRepository: Repository<Inventory>,
         @InjectRepository(BatchInfo)
-        private readonly batchInfoRepository: Repository<BatchInfo>
+        private readonly batchInfoRepository: Repository<BatchInfo>,
+        private schedulerRegistry: SchedulerRegistry
     ) {}
 
     async getInventory(): Promise<Inventory[]> {
@@ -86,7 +89,27 @@ export class Task3Service {
         }
     }
 
-    @Cron(CronExpression.EVERY_30_SECONDS)
+    //=== Cron Job Setup ===
+    // This method is called when the module is initialized
+    onModuleInit() {
+        const isProd = process.env.NODE_ENV === "production"
+        const cronTime = isProd
+            ? CronExpression.EVERY_30_SECONDS
+            : CronExpression.EVERY_10_SECONDS
+        this.addCronJob("sync-db-with-contact", cronTime)
+    }
+
+    // This method is used to register a cron job
+    addCronJob(name: string, cronTime: string) {
+        const job = new CronJob(cronTime, () => {
+            this.handleCron()
+        })
+        this.schedulerRegistry.addCronJob(name, job)
+        job.start()
+        console.log(`Cron job ${name} started with time: ${cronTime}`)
+    }
+
+    // This method is called when the cron job is triggered
     handleCron() {
         console.log(`Cron job running at ${new Date().toISOString()}`)
     }
