a9c44c346a8888324abbd3a36d7a170c97c517da
Samonto Karmaker <samonto0726@gmail.com>
2025-05-27 14:00:57 +0600

    feat: update Task3 contract to inherit Ownable and restrict access to create and update functions

--- anonymized-compliance-checker-web3/contracts/task3.sol
+++ anonymized-compliance-checker-web3/contracts/task3.sol
@@ -1,12 +1,20 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Task3 Contract
 /// @author Samanta Karmaker
 /// @notice This contract emits events for creation and update hashes, storing only the latest hashes on-chain.
 /// @dev Only the latest hashes are stored; full history is available off-chain via events.
 
-contract Task3 {
+contract Task3 is Ownable {
+    /**
+     * @notice Initializes the contract setting the deployer as the initial owner.
+     * @param initialOwner The address to be set as the initial owner of the contract.
+     */
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
     /// @notice Stores the latest creation hash
     string public latestCreationHash;
 
@@ -26,7 +34,7 @@ contract Task3 {
     /// @notice Sets the latest creation hash and emits an event
     /// @param id the creation batch identifier
     /// @param hash The hash value to store and emit
-    function create(uint256 id, string memory hash) external {
+    function create(uint256 id, string memory hash) external onlyOwner {
         latestCreationHash = hash;
         emit CreationHashUpdated(id, hash);
     }
@@ -34,7 +42,7 @@ contract Task3 {
     /// @notice Sets the latest update hash and emits an event
     /// @param id the update batch identifier
     /// @param hash The hash value to store and emit
-    function update(uint256 id, string memory hash) external {
+    function update(uint256 id, string memory hash) external onlyOwner {
         latestUpdateHash = hash;
         emit UpdateHashUpdated(id, hash);
     }
