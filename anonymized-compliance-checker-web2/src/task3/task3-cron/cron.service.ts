7023c7890ac9c038cae4722120e45da6506b7d32
Samonto Karmaker <samonto0726@gmail.com>
2025-05-27 19:17:45 +0600

    fix: update CronService to use async methods for cron job handling and adjust cron timing

--- anonymized-compliance-checker-web2/src/task3/task3-cron/cron.service.ts
+++ anonymized-compliance-checker-web2/src/task3/task3-cron/cron.service.ts
@@ -1,29 +1,33 @@
 import { Injectable, OnModuleInit } from "@nestjs/common"
 import { CronJob } from "cron"
 import { CronExpression, SchedulerRegistry } from "@nestjs/schedule"
+import { BatchService } from "../task3-batch/batch.service"
 
 @Injectable()
 export class CronService implements OnModuleInit {
-    constructor(private schedulerRegistry: SchedulerRegistry) {}
+    constructor(
+        private schedulerRegistry: SchedulerRegistry,
+        private readonly batchService: BatchService
+    ) {}
 
     // This method is called when the module is initialized
-    onModuleInit() {
+    async onModuleInit() {
         const isProd = process.env.NODE_ENV === "production"
         const cronTime = isProd
-            ? CronExpression.EVERY_30_SECONDS
-            : CronExpression.EVERY_10_SECONDS
+            ? CronExpression.EVERY_30_MINUTES
+            : CronExpression.EVERY_10_MINUTES
         this.addCronJob("sync-db-with-contact", cronTime)
 
         if (!isProd) {
             console.log(`Cron job is running in development mode`)
-            this.handleCron()
+            await this.handleCron()
         }
     }
 
     // This method is used to register a cron job
     private addCronJob(name: string, cronTime: string) {
-        const job = new CronJob(cronTime, () => {
-            this.handleCron()
+        const job = new CronJob(cronTime, async () => {
+            await this.handleCron()
         })
         this.schedulerRegistry.addCronJob(name, job)
         job.start()
@@ -31,7 +35,11 @@ export class CronService implements OnModuleInit {
     }
 
     // This method is called when the cron job is triggered
-    private handleCron() {
-        console.log(`Cron job running at ${new Date().toISOString()}`)
+    private async handleCron() {
+        console.log("Cron job triggered!")
+        await this.batchService.sendCreationBatch()
+        console.log("Batch creation processing completed!")
+        await this.batchService.sendUpdateBatch()
+        console.log("Batch update processing completed!")
     }
 }
