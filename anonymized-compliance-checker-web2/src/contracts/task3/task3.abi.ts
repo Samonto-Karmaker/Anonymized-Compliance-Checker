f2a72f5cd947faee62cd86e795019271d6d86890
Samonto Karmaker <samonto0726@gmail.com>
2025-05-27 16:22:52 +0600

    feat: add constructor and ownership management functions to task3 ABI

--- anonymized-compliance-checker-web2/src/contracts/task3/task3.abi.ts
+++ anonymized-compliance-checker-web2/src/contracts/task3/task3.abi.ts
@@ -1,5 +1,22 @@
 export const abi = [
     {
+        inputs: [
+            { internalType: "address", name: "initialOwner", type: "address" },
+        ],
+        stateMutability: "nonpayable",
+        type: "constructor",
+    },
+    {
+        inputs: [{ internalType: "address", name: "owner", type: "address" }],
+        name: "OwnableInvalidOwner",
+        type: "error",
+    },
+    {
+        inputs: [{ internalType: "address", name: "account", type: "address" }],
+        name: "OwnableUnauthorizedAccount",
+        type: "error",
+    },
+    {
         anonymous: false,
         inputs: [
             {
@@ -16,6 +33,25 @@ export const abi = [
             },
         ],
         name: "CreationHashUpdated",
+        type: "event",
+    },
+    {
+        anonymous: false,
+        inputs: [
+            {
+                indexed: true,
+                internalType: "address",
+                name: "previousOwner",
+                type: "address",
+            },
+            {
+                indexed: true,
+                internalType: "address",
+                name: "newOwner",
+                type: "address",
+            },
+        ],
+        name: "OwnershipTransferred",
         type: "event",
     },
     {
@@ -59,6 +95,29 @@ export const abi = [
         name: "latestUpdateHash",
         outputs: [{ internalType: "string", name: "", type: "string" }],
         stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "owner",
+        outputs: [{ internalType: "address", name: "", type: "address" }],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "renounceOwnership",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        inputs: [
+            { internalType: "address", name: "newOwner", type: "address" },
+        ],
+        name: "transferOwnership",
+        outputs: [],
+        stateMutability: "nonpayable",
         type: "function",
     },
     {
