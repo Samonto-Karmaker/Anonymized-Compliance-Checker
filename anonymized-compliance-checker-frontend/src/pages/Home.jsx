360a85d41fd4bc0f43f20d68c4533bc4cfdae118
AlFahimBinFaruk <alfahim.ps@gmail.com>
2025-05-28 19:08:27 +0600

    feat:added verification UI for task3

--- anonymized-compliance-checker-frontend/src/pages/Home.jsx
+++ anonymized-compliance-checker-frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Task1Component from "../components/Task1Component";
 import Task2Component from "../components/Task2Component";
 import CreateInventoryComponent from "../components/Task3/CreateInventoryComponent";
 import UpdateDisbursementDateComponent from "../components/Task3/UpdateDisbursementDateComponent";
+import VerifyComponent from "../components/Task3/VerifyComponent";
 
 const Home = () => {
   return (
@@ -20,6 +21,7 @@ const Home = () => {
         <h1 className="text-2xl font-bold text-gray-800 mb-6">Task 3</h1>
         <CreateInventoryComponent />
         <UpdateDisbursementDateComponent />
+        <VerifyComponent/>
       </div>
     </>
   );
